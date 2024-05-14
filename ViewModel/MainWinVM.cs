1ad63da58846893666e27fa9245fb339ac03bf81
raheli19 <rahamar@g.jct.ac.il>
2024-05-14 18:09:42 +0300

    fixes

--- ViewModel/MainWinVM.cs
+++ ViewModel/MainWinVM.cs
@@ -1,8 +1,6 @@
 ﻿using BL;
 using BO;
-using BO.Flights;
 using FlightsMap.ViewModel.Commands;
-using FlightsMap.Windows;
 using Microsoft.Maps.MapControl.WPF;
 using System;
 using System.Collections.Generic;
@@ -63,45 +61,6 @@ namespace FlightsMap.ViewModel
 
 
         public object Push { get; private set; }
-
-        private void UpdateFlight(FlightInfoPartial selected)
-        {
-            var Flight = bl.GetFlightDetail(selected.SourceId);
-
-            // Update map
-            if (Flight != null)
-            {
-                var OrderedPlaces = (from f in Flight.trail
-                                     orderby f.ts
-                                     select f).ToList<Trail>();
-
-                addNewPolyLine(OrderedPlaces);
-
-                Trail CurrentPlace = null;
-                ControlTemplate template = (ControlTemplate)MW.FindResource("location");
-                Pushpin PinCurrent = new Pushpin { ToolTip = selected.FlightCode, Template = template };
-                if (Flight.airport.origin != null)
-                {
-                    Pushpin PinOrigin = new Pushpin { ToolTip = Flight.airport.origin.name, Template = template };
-
-                    PositionOrigin origin = new PositionOrigin { X = 0.4, Y = 0.4 };
-                    MapLayer.SetPositionOrigin(PinCurrent, origin);
-
-
-                    CurrentPlace = OrderedPlaces.Last<Trail>();
-                    var PlaneLocation = new Location { Latitude = CurrentPlace.lat, Longitude = CurrentPlace.lng };
-                    PinCurrent.Location = PlaneLocation;
-
-
-                    CurrentPlace = OrderedPlaces.First<Trail>();
-                    PlaneLocation = new Location { Latitude = CurrentPlace.lat, Longitude = CurrentPlace.lng };
-                    PinOrigin.Location = PlaneLocation;
-                    Origin.Clear();
-                    Origin.Add(PinOrigin);
-                }
-
-            }
-        }
         void addNewPolyLine(List<Trail> Route)
         {
             MapPolyline polyline = new MapPolyline();
@@ -115,37 +74,6 @@ namespace FlightsMap.ViewModel
             {
                 polyline.Locations.Add(new Location(item.lat, item.lng));
             }
-        }
-        private void Push_MouseEnterOut(object sender, MouseButtonEventArgs e, FlightInfoPartial flight)
-        {
-            bl.AddWatch(new Watch { Date = DateTime.Now, Destination = flight.Destination, FlightNumber = flight.FlightCode, Origin = flight.Source, UserName = MyUser.UserId });
-            var p = (Pushpin)sender;
-            ControlTemplate template = (ControlTemplate)MW.FindResource("land");
-            p.Template = template;
-            UpdateFlight(flight);
-            // open details window
-            FlightDetailsWinVM fdvm = new FlightDetailsWinVM(flight);
-            WinFlightDetails wfd = new WinFlightDetails();
-            fdvm.WFD = wfd;
-            fdvm.WFD.DataContext = fdvm;
-            fdvm.WFD.Show();
-        }
-        private void Push_MouseEnterIn(object sender, MouseButtonEventArgs e, FlightInfoPartial flight)
-        {
-            bl.AddWatch(new Watch { Date = DateTime.Now, Destination = flight.Destination, FlightNumber = flight.FlightCode, Origin = flight.Source, UserName = MyUser.UserId });
-            var p = (Pushpin)sender;
-            ControlTemplate template = (ControlTemplate)MW.FindResource("takeoff");
-            p.Template = template;
-            UpdateFlight(flight);
-          
-
-            // open details window
-            FlightDetailsWinVM fdvm = new FlightDetailsWinVM(flight);
-            //fdvm.FlightPartial = flight;
-            WinFlightDetails wfd = new WinFlightDetails();
-            fdvm.WFD = wfd;
-            fdvm.WFD.DataContext = fdvm;
-            fdvm.WFD.Show();
         }
 
        
